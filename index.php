2188e60084a607257f54fdb1287fd2af9521c523
Qaiser Shakoor <link2qaiser@gmail.com>
2022-09-07 14:31:32 +0500

    fix the position of table of content

--- index.php
+++ index.php
@@ -9,7 +9,20 @@
    License: GPL2
    */
 if ( ! is_admin() ) {
-   
+   add_filter( 'the_content', 'update_post_conent', 1 );
+
+   function update_post_conent( $content ) {
+      for($i = 1; $i <= 5; $i++) {
+         $headings = [];
+         preg_match_all('/<h'.$i.'>.*<\/h'.$i.'>/i',$content,$html);
+         if(isset($html[0])) {
+            foreach ($html[0] as $key => $value) {
+               $content = preg_replace('/(<h3\b[^><]*)>.*'.strip_tags($value).'/i', '$1 id="'.strip_tags($value).'">'.strip_tags($value), $content);
+            }
+         }
+      }
+      return $content;
+   }
    add_shortcode( 'sm-content', 'load_conent' );
    function getHeadings($string) {
       $results = [];
@@ -44,24 +57,13 @@ if ( ! is_admin() ) {
       $data = array_reduce($list, function ($a, $b) {
           return @$a['total'] > $b['total'] ? $a : $b ;
       });
+      ob_start();
       echo '<ul class="sm-tb-con">';
       foreach($data['headings'] as $key=>$value) {
          echo '<li><a href="#'.$value.'">'.$value.'</a></li>';
       }
       echo '</ul>';
+      return ob_get_clean();
    }
-   add_filter( 'the_content', 'update_post_conent', 1 );
-
-   function update_post_conent( $content ) {
-      for($i = 1; $i <= 5; $i++) {
-         $headings = [];
-         preg_match_all('/<h'.$i.'>.*<\/h'.$i.'>/i',$content,$html);
-         if(isset($html[0])) {
-            foreach ($html[0] as $key => $value) {
-               $content = preg_replace('/(<h3\b[^><]*)>.*'.strip_tags($value).'/i', '$1 id="'.strip_tags($value).'">'.strip_tags($value), $content);
-            }
-         }
-      }
-      return $content;
-   }
+   
 }
