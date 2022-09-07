fd849ec37e028c6e8a458f4bec5aaf667e17ff51
Qaiser Shakoor <link2qaiser@gmail.com>
2022-09-07 14:18:06 +0500

    finish the v2

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
    License: GPL2
    */
 if ( ! is_admin() ) {
+   
    add_shortcode( 'sm-content', 'load_conent' );
    function getHeadings($string) {
       $results = [];
@@ -27,17 +28,40 @@ if ( ! is_admin() ) {
       return $results;
    }
    function load_conent($atts) {
-      echo preg_replace('/(.*?):/', "<font color=#F00>$1</font>:", $output);
+
+      
+      /*
+      Get the current post
+      */
 
       $post = get_post();
+      
+      /*
+      Get all headings
+      */
       $list = getHeadings($post->post_content);
+
       $data = array_reduce($list, function ($a, $b) {
           return @$a['total'] > $b['total'] ? $a : $b ;
       });
       echo '<ul class="sm-tb-con">';
       foreach($data['headings'] as $key=>$value) {
-         echo '<li><a "#'.$value.'">'.$value.'</a></li>';
+         echo '<li><a href="#'.$value.'">'.$value.'</a></li>';
       }
       echo '</ul>';
+   }
+   add_filter( 'the_content', 'update_post_conent', 1 );
+
+   function update_post_conent( $content ) {
+      for($i = 1; $i <= 5; $i++) {
+         $headings = [];
+         preg_match_all('/<h'.$i.'>.*<\/h'.$i.'>/i',$content,$html);
+         if(isset($html[0])) {
+            foreach ($html[0] as $key => $value) {
+               $content = preg_replace('/(<h3\b[^><]*)>.*'.strip_tags($value).'/i', '$1 id="'.strip_tags($value).'">'.strip_tags($value), $content);
+            }
+         }
+      }
+      return $content;
    }
 }
